40e26ced0f893b9a53d33a019d4556cd644577c0
Jessica Monteiro Bueno <monteirojeh42@gmail.com>
2023-07-03 20:07:48 -0300

    Alterações de JS, CSS e HTML;

--- js/main.js
+++ js/main.js
@@ -1,42 +1,139 @@
-/* 
-
-O que precisa ser feito:
-
- Página index.html quando clicado em "Comprar Agora", precisa direcionar para a página de checkout e la conter os itens adicionados;
-
- Página checkout.html = 
- 1) Carregar a imagem do produto;
- 2) Carregar o nome do produto;
- 3) Carregar o preço do produto;
- 4) Fazer a função de soma e subtração da quantidade;
- 5) Calcular o total de cada produto;
- 6) Calcular o total de todos os produtos;
- 7) Quando clicado em "Compre Agora" pensar em alguma ação de pagamento ou apenas dizer que o cliente vai receber o link de pagamento.
-
-*/
-
-/* IDs de HTML selecionados */
-let imagem = document.querySelectorAll("image");
-let produto = document.querySelectorAll("product");
-let preco = document.querySelectorAll("price").value;
-let quantidade = document.querySelectorAll("qtd").value;
-let total = document.getElementById("total").valeu;
-
-var somando;
-var subtraindo;
-var resultado;
-
-/*Função global */
-function comprar(event) {
-
-  function soma(value) {
-    //   soma = qtd * preco;
-    somando = document.querySelectorAll("qtd").value * document.querySelectorAll("price");
+const items = [
+  {
+    id: 0,
+    img: "images/produto-1.png",
+    title: "Brinco Semijoia Banhada com Micro Zircônia",
+    price: '45,00',
+    quantidade: 0,
+  },
+  {
+    id: 1,
+    img: "images/produto-2.png",
+    title: "Pulseira Semijoia Banhada com Micro Zircônia",
+    price: '56,00',
+    quantidade: 0,
+  },
+  {
+    id: 2,
+    img: "images/produto-3.png",
+    title: "Brinco Borboleta Infantil Antialérgico",
+    price: '39,00',
+    quantidade: 0,
+  },
+  {
+    id: 3,
+    img: "images/produto-4.png",
+    title: "Colar Prata Semijoia Banhada com Micro Zircônia",
+    price: '68,00',
+    quantidade: 0,
+  },
+  {
+    id: 4,
+    img: "images/produto-5.png",
+    title: "Colar Dourado Semijoia Banhada com Micro Zircônia",
+    price: '87,00',
+    quantidade: 0,
+  },
+  {
+    id: 5,
+    img: "images/produto-6.png",
+    title: "Brinco Infantil Kello Kitty Antialérgico",
+    price: '49,90',
+    quantidade: 0,
+  },
+  {
+    id: 6,
+    img: "images/produto-7.png",
+    title: "Brinco Argola Semijoia Banhada",
+    price: '69,90',
+    quantidade: 0,
+  },
+  {
+    id: 7,
+    img: "images/produto-8.png",
+    title: "Brinco Infantil Joaninha Antialérgico",
+    price: '45,90',
+    quantidade: 0,
   }
+];
 
-  function subtrai(value) {
-    //   subtracao = qtd * preco - preco;
-    subtraindo = somando - document.querySelectorAll("price");
-  }
-  
+function inicializarLoja() {
+  var containerProdutos = document.getElementById("products");
+  items.map((val) => {
+    containerProdutos.innerHTML += `
+    
+    <div class="card-produto">
+      <img src="`+val.img+`" alt>
+        <div>
+          <h3 class="heading-16">`+val.title+`</h3>
+        </div>
+        <div>
+          <h3 class="heading-18 w-700">R$ `+val.price+`</h3>
+        </div>
+          <a id="btn" class="btn-default heading-20 w-700" key="`+val.id+`" href="#">Comprar Agora</a>
+    </div>
+                
+    `;
+  })
 }
+
+inicializarLoja()
+
+function atualizarCarrinho() {
+  var containerCarrinho = document.getElementById("cart");
+  containerCarrinho.innerHTML= " ";
+  items.map((val) => {
+    if(val.quantidade > 0) {
+    containerCarrinho.innerHTML += 
+    
+    `
+      <div class="cart-box">
+        <tbody>
+            <tr>
+                <td>
+                    <img width="100" src="`+val.img+`" alt>
+                </td>
+                <td><h3 class="heading-16" href="#">`+val.title+`</h3>
+                    <div class="mobile-cart-content">
+                        <div class="col">
+                            <div class="qty-box">
+                                <h2 class="heading-16">`+val.quantidade+`</h2>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <h2 class="heading-16 w-700">R$ `+val.price+`</h2>
+                </td>
+                <td>
+                    <h2 id="total"class="heading-16 w-700">R$ `+val.price+`</h2>
+                </td>
+            </tr>
+        </tbody>
+      </div> 
+    `;
+    }
+  });
+}
+
+
+var links = document.getElementsByTagName('a');
+
+for(var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    event.preventDefault();
+
+    let key = this.getAttribute('key');
+    items[key].quantidade++;
+    atualizarCarrinho();
+
+    // Atualizar o valor;
+    //Colocar o valor em reais 50,00 (transformar em number na soma);
+
+  })
+
+}
+
+// Forma de pagamento - verificar como usar o link do pagueseguro.
+
+
