c3f5c60ba260aeb57b3f67c0c63cb5333d0be46f
Jessica Monteiro Bueno <monteirojeh42@gmail.com>
2023-07-11 08:17:06 -0300

    Começo de criação função soma de todos os itens;

--- js/main.js
+++ js/main.js
@@ -3,92 +3,99 @@ const items = [
     id: 0,
     img: "images/produto-1.png",
     title: "Brinco Semijoia Banhada com Micro Zircônia",
-    price: '45,00',
+    price: "45,00",
     quantidade: 0,
   },
   {
     id: 1,
     img: "images/produto-2.png",
     title: "Pulseira Semijoia Banhada com Micro Zircônia",
-    price: '56,00',
+    price: "56,00",
     quantidade: 0,
   },
   {
     id: 2,
     img: "images/produto-3.png",
     title: "Brinco Borboleta Infantil Antialérgico",
-    price: '39,00',
+    price: "39,00",
     quantidade: 0,
   },
   {
     id: 3,
     img: "images/produto-4.png",
     title: "Colar Prata Semijoia Banhada com Micro Zircônia",
-    price: '68,00',
+    price: "68,00",
     quantidade: 0,
   },
   {
     id: 4,
     img: "images/produto-5.png",
     title: "Colar Dourado Semijoia Banhada com Micro Zircônia",
-    price: '87,00',
+    price: "87,00",
     quantidade: 0,
   },
   {
     id: 5,
     img: "images/produto-6.png",
     title: "Brinco Infantil Kello Kitty Antialérgico",
-    price: '49,90',
+    price: "49,90",
     quantidade: 0,
   },
   {
     id: 6,
     img: "images/produto-7.png",
     title: "Brinco Argola Semijoia Banhada",
-    price: '69,90',
+    price: "69,90",
     quantidade: 0,
   },
   {
     id: 7,
     img: "images/produto-8.png",
     title: "Brinco Infantil Joaninha Antialérgico",
-    price: '45,90',
+    price: "45,90",
     quantidade: 0,
-  }
+  },
 ];
 
 function inicializarLoja() {
   var containerProdutos = document.getElementById("products");
   items.map((val) => {
-    containerProdutos.innerHTML += `
+    containerProdutos.innerHTML +=
+      `
     
     <div class="card-produto">
-      <img src="`+val.img+`" alt>
+      <img src="` +
+      val.img +
+      `" alt>
         <div>
-          <h3 class="heading-16">`+val.title+`</h3>
+          <h3 class="heading-16">` +
+      val.title +
+      `</h3>
         </div>
         <div>
-          <h3 class="heading-18 w-700">R$ `+val.price+`</h3>
+          <h3 class="heading-18 w-700">R$ ` +
+      val.price +
+      `</h3>
         </div>
-          <a id="btn" class="btn-default heading-20 w-700" key="`+val.id+`" href="#">Comprar Agora</a>
+          <a id="btn" class="btn-default heading-20 w-700" key="` +
+      val.id +
+      `" href="#">Comprar Agora</a>
     </div>
                 
     `;
-  })
+  });
 }
 
-inicializarLoja()
+inicializarLoja();
 
 function atualizarCarrinho() {
   var containerCarrinho = document.getElementById("cart");
-  containerCarrinho.innerHTML= " ";
+  containerCarrinho.innerHTML = " ";
   items.map((val) => {
-    if(val.quantidade > 0) {
+    if (val.quantidade > 0) {
       var total = parseFloat(val.price) * val.quantidade;
 
-    containerCarrinho.innerHTML += 
-    
-    `
+      containerCarrinho.innerHTML += `
       <div class="cart-box">
         <tbody>
             <tr>
@@ -105,10 +112,14 @@ function atualizarCarrinho() {
                     </div>
                 </td>
                 <td>
-                    <h2 class="heading-16 w-700">R$ ${parseFloat(val.price).toFixed(2)}</h2>
+                    <h2 class="heading-16 w-700">R$ ${parseFloat(
+                      val.price
+                    ).toFixed(2)}</h2>
                 </td>
                 <td>
-                    <h2 id="total"class="heading-16 w-700">R$ ${total.toFixed(2)}</h2>
+                    <h2 id="total"class="heading-16 w-700">R$ ${total.toFixed(
+                      2
+                    )}</h2>
                 </td>
             </tr>
         </tbody>
@@ -118,34 +129,58 @@ function atualizarCarrinho() {
   });
 }
 
+// Não está inserindo no html, verificar
 
-var links = document.getElementsByTagName('a');
+function atualizarTotalFinal() {
+  var containerTotalFinal = document.getElementById("total");
+    containerTotalFinal.innerHTML += 
+    `
+      <div class="table-responsive-md">
+          <table class="table cart-table ">
+              <tfoot>
+                  <tr>
+                      <td class="heading-18 color-gold">Total:</td>
+                      <td>
+                          <h2 class="heading-24 w-700 color-gold">R$1000,00</h2>
+                      </td>
+                  </tr>
+              </tfoot>
+          </table>
+      </div>
+    `;
+  ;
+}
 
-for(var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function(event) {
+
+var links = document.getElementsByTagName("a");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function (event) {
     event.preventDefault();
 
-    let key = this.getAttribute('key');
+    let key = this.getAttribute("key");
     items[key].quantidade++;
     atualizarCarrinho();
 
     var containerCarrinho = document.getElementById("cart");
-    containerCarrinho.scrollIntoView({ behavior: 'smooth'});
+    containerCarrinho.scrollIntoView({ behavior: "smooth" });
 
-    // Atualizar o valor;
+    
     //Colocar o valor em reais 50,00 (transformar em number na soma);
-
   });
-
 }
+
 
 
 // O que ainda falta:
 
-// Quando adicionado o produto, dar scroll para o carrinho abaixo; - Feito
-// Calcular o total de todos os produtos; - Feito
+
+
+// Quando adicionado o produto, dar scroll para o carrinho abaixo; - Feito Karol
+// Calcular o total de todos os produtos; - Feito Karol
+// Terminar o tatalFinal() - Jéssica começou
+// Ter a opção de excluir os produtos do carrinho;
 // Forma de pagamento - verificar como usar o link do pagueseguro;
 // Guardar os dados da compra no localStorage;
 // Fazer o footer (colocar logotipo);
 // Colocar os links do header.
-
